84a622f10563b76fb5014f8013c341cc386e29ed
motivational-dragon <116593274+motivational-dragon@users.noreply.github.com>
2024-04-06 12:38:05 +0200

    Aligned item in jei menu Shapeless recipe now show as shapeless in JEI
    Fix missingjei api for fabric when compiling

--- Common/src/main/java/mod/motivationaldragon/potionblender/integration/BrewingCauldronJeiCategory.java
+++ Common/src/main/java/mod/motivationaldragon/potionblender/integration/BrewingCauldronJeiCategory.java
@@ -1,10 +1,8 @@
 package mod.motivationaldragon.potionblender.integration;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
-import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.ingredients.IIngredientType;
 import mezz.jei.api.recipe.IFocusGroup;
@@ -14,13 +12,15 @@ import mezz.jei.api.recipe.category.IRecipeCategory;
 import mod.motivationaldragon.potionblender.Constants;
 import mod.motivationaldragon.potionblender.block.PotionBlenderBlock;
 import mod.motivationaldragon.potionblender.recipes.BrewingCauldronRecipe;
-import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 public class BrewingCauldronJeiCategory implements IRecipeCategory<BrewingCauldronRecipe> {
+
+	//TODO make potion display properly and not ass uncraftable potion
+
 
 	public static final ResourceLocation UID = new ResourceLocation(Constants.MOD_ID, "brewing_cauldron");
 	public static final ResourceLocation BACKGROUND_TEXTURE = new ResourceLocation(Constants.MOD_ID, "textures/gui/jei/brewing_cauldron_gui.png");
@@ -31,8 +31,8 @@ public class BrewingCauldronJeiCategory implements IRecipeCategory<BrewingCauldr
 	private final IDrawable icon;
 
 	public BrewingCauldronJeiCategory(IGuiHelper guiHelper) {
-		this.background = guiHelper.createDrawable(BACKGROUND_TEXTURE,0,0,176,85);
-		this.icon = guiHelper.createDrawableIngredient(VanillaTypes.ITEM_STACK,new ItemStack(PotionBlenderBlock.BREWING_CAULDRON_ITEM));
+		this.background = guiHelper.createDrawable(BACKGROUND_TEXTURE, 0, 0, 166, 74);
+		this.icon = guiHelper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(PotionBlenderBlock.BREWING_CAULDRON_ITEM));
 	}
 
 	/**
@@ -76,28 +76,6 @@ public class BrewingCauldronJeiCategory implements IRecipeCategory<BrewingCauldr
 	}
 
 	/**
-	 * Draw extras or additional info about the recipe.
-	 * Use the mouse position for things like button highlights.
-	 * Tooltips are handled by {@link #getTooltipStrings(Object, IRecipeSlotsView, double, double)}
-	 *
-	 * @param recipe          the current recipe being drawn.
-	 * @param recipeSlotsView a view of the current recipe slots being drawn.
-	 * @param guiGraphics     the current {@link GuiGraphics} for rendering.
-	 * @param mouseX          the X position of the mouse, relative to the recipe.
-	 * @param mouseY          the Y position of the mouse, relative to the recipe.
-	 * @see IDrawable for a simple class for drawing things.
-	 * @see IGuiHelper for useful functions.
-	 * @see IRecipeSlotsView for information about the ingredients that are currently being drawn.
-	 * @since 9.3.0
-	 */
-	@Override
-	public void draw(@NotNull BrewingCauldronRecipe recipe, @NotNull IRecipeSlotsView recipeSlotsView, @NotNull GuiGraphics guiGraphics, double mouseX, double mouseY) {
-		IRecipeCategory.super.draw(recipe, recipeSlotsView, guiGraphics, mouseX, mouseY);
-		RenderSystem.enableBlend();
-		RenderSystem.disableBlend();
-	}
-
-	/**
 	 * Sets all the recipe's ingredients by filling out an instance of {@link IRecipeLayoutBuilder}.
 	 * This is used by JEI for lookups, to figure out what ingredients are inputs and outputs for a recipe.
 	 *
@@ -109,11 +87,13 @@ public class BrewingCauldronJeiCategory implements IRecipeCategory<BrewingCauldr
 	@Override
 	public void setRecipe(@NotNull IRecipeLayoutBuilder builder, BrewingCauldronRecipe recipe, @NotNull IFocusGroup focuses) {
 
-		for(int i = 0; i < recipe.getIngredients().size(); i++){
-			builder.addSlot(RecipeIngredientRole.INPUT, 1 + i * 18, 65).addIngredients(recipe.getIngredients().get(i));
+		if (!recipe.isOrdered()) {
+			builder.setShapeless();
 		}
 
-
-		builder.addSlot(RecipeIngredientRole.OUTPUT, 80, 65).addItemStack(recipe.getOutput());
+		for (int i = 0; i < recipe.getIngredients().size(); i++) {
+			builder.addSlot(RecipeIngredientRole.INPUT, 38 + i * 18, 4).addIngredients(recipe.getIngredients().get(i));
+		}
+		builder.addSlot(RecipeIngredientRole.OUTPUT, 74, 52).addItemStack(recipe.getOutput());
 	}
 }
